3ef26b9cbcab4c438feb8c43fe90a62cfd41888e
FabiBr <fabian.br.1992@gmail.com>
2014-08-20 17:36:56 +0200

    changes to dbController

--- 4Gewinnt/src/de/ur/mi/android/excercises/starter/DBController.java
+++ 4Gewinnt/src/de/ur/mi/android/excercises/starter/DBController.java
@@ -1,5 +1,28 @@
 package de.ur.mi.android.excercises.starter;
 
-public class DBController {
+import java.sql.*;
 
+import android.content.Context;
+import android.database.sqlite.*;
+
+
+public class DBController extends SQLiteOpenHelper {
+	
+
+	public DBController(Context context) {
+		super(context, "dbname", null, 1);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+
+	}
 }
