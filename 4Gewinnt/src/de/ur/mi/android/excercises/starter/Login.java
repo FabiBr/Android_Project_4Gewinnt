28e26917f3a8ccd3196946f39d4e5fd659721fbc
FabiBr <fabian.br.1992@gmail.com>
2014-09-14 19:33:17 +0200

    networking + new app structure combined

--- 4Gewinnt/src/de/ur/mi/android/excercises/starter/Login.java
+++ 4Gewinnt/src/de/ur/mi/android/excercises/starter/Login.java
@@ -1,5 +1,9 @@
 package de.ur.mi.android.excercises.starter;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,35 +15,62 @@ import android.widget.Toast;
 
 public class Login extends Activity {
 
+	Socket socket = null;
+	private static final String SERVER_IP = null;
+	private static final int SERVERPORT = 4444;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
 		Button logbutton = (Button) findViewById(R.id.logcheckbutton);
 		logbutton.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				try {
-					if(checkUser()){
-					startActivity(new Intent(Login.this, Overview.class));}
-					else Toast.makeText(Login.this, "Falsche Zugangsdaten. Eventuell neu registrieren?", Toast.LENGTH_LONG).show();
+					if (checkUser()) {
+						startActivity(new Intent(Login.this, Overview.class));
+					} else
+						Toast.makeText(
+								Login.this,
+								"Falsche Zugangsdaten. Eventuell neu registrieren?",
+								Toast.LENGTH_LONG).show();
 				} catch (Exception e) {
 				}
-				
+
 			}
 		});
-		
-			
+
 	}
 
 	private boolean checkUser() {
-		String s_name,s_pw;
+		String s_name, s_pw;
 		EditText name = (EditText) findViewById(R.id.editText4);
 		EditText pw = (EditText) findViewById(R.id.editText5);
 		s_name = name.getText().toString();
 		s_pw = pw.getText().toString();
-		//TODO: aus Datenbank user abrufen
+		// TODO: aus Datenbank user abrufen
 		return false;
+	}
+
+	class ClientThread implements Runnable {
+
+
+		@Override
+		public void run() {
+
+			try {
+				// InetAddress serverAddr = InetAddress.getByName(SERVER_IP);
+
+				socket = new Socket(SERVER_IP, SERVERPORT);
+				System.out.println("gr8 success very nice");
+
+			} catch (UnknownHostException e1) {
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}
 	}
 }
--- 4Gewinnt/src/de/ur/mi/android/excercises/starter/Register.java
+++ 4Gewinnt/src/de/ur/mi/android/excercises/starter/Register.java
@@ -1,5 +1,12 @@
 package de.ur.mi.android.excercises.starter;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,31 +17,60 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class Register extends Activity {
+	Socket socket = null;
+
+	// zum Testen: Server Main() einfach in Eclipse laugen lassen. Beim debuggen
+	// mit echtem gerät bei Server IP die eignene IP eingeben console ->
+	// ipconfig
+	// beim testen mit virtual device einfach localhost verwenden
+	private static final String SERVER_IP = null;
+	private static final int SERVERPORT = 4444;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_register);
+		new Thread(new ClientThread()).start();
 		Button logbutton = (Button) findViewById(R.id.regcheckbutton);
 		logbutton.setOnClickListener(new OnClickListener() {
 
-				@Override
-				public void onClick(View v) {
-					try {
-						if(checkPassword()){
-						startActivity(new Intent(Register.this,
-								Overview.class));}
-						else Toast.makeText(Register.this, "Bitte zweimal das gleiche Passwort eingeben", Toast.LENGTH_LONG).show();
-					} catch (Exception e) {
-					}
-				}
-			});
-		}
+			@Override
+			public void onClick(View v) {
+				try {
+					if (checkPassword()) {
+						try {
+							EditText username = (EditText) findViewById(R.id.editText1);
+							EditText password = (EditText) findViewById(R.id.editText2);
+							String name = username.getText().toString();
+							String pw = password.getText().toString();
 
-	
+							PrintWriter out = new PrintWriter(
+									new BufferedWriter(new OutputStreamWriter(
+											socket.getOutputStream())), true);
+							out.println(name + " ," + pw);
+							out.flush();
+
+						} catch (UnknownHostException e) {
+							e.printStackTrace();
+						} catch (IOException e) {
+							e.printStackTrace();
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+
+						startActivity(new Intent(Register.this, Overview.class));
+					} else
+						Toast.makeText(Register.this,
+								"Bitte zweimal das gleiche Passwort eingeben",
+								Toast.LENGTH_LONG).show();
+				} catch (Exception e) {
+				}
+			}
+		});
+	}
 
 	private boolean checkPassword() {
-		String s_name,s_pw,s_pwwh;
+		String s_name, s_pw, s_pwwh;
 		EditText name = (EditText) findViewById(R.id.editText1);
 		EditText pw = (EditText) findViewById(R.id.editText2);
 		EditText pwwh = (EditText) findViewById(R.id.editText3);
@@ -43,9 +79,28 @@ public class Register extends Activity {
 		s_pwwh = pwwh.getText().toString();
 		System.out.println(s_pw);
 		System.out.println(s_pwwh);
-		//TODO: in Datenbank speichern
-		if (s_pw.equals(s_pwwh)) return true;
+		// TODO: in Datenbank speichern
+		if (s_pw.equals(s_pwwh))
+			return true;
 		return false;
 	}
 
+	class ClientThread implements Runnable {
+
+		@Override
+		public void run() {
+
+			try {
+				// InetAddress serverAddr = InetAddress.getByName(SERVER_IP);
+
+				socket = new Socket(SERVER_IP, SERVERPORT);
+				System.out.println("gr8 success very nice");
+
+			} catch (UnknownHostException e1) {
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}
+	}
 }
